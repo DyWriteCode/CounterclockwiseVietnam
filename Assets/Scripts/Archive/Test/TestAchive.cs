18407d15a96e7eccd09cf709122ad58463a13a40
DyAdmin <DyAdmin@w1k3z.onmicrosoft.com>
2024-04-21 22:37:06 +0800

    repaired some bugs

--- Assets/Scripts/Archive/Test/TestAchive.cs
+++ Assets/Scripts/Archive/Test/TestAchive.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,4 +10,6 @@ using UnityEngine;
 public class TestAchive : ArchiveData
 {
     public string hello;
+    public List<string> tags;
+    public Dictionary<string, string> tags2;
 }
