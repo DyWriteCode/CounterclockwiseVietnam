1689bcc1ab2efc44969740aa5174fe77e280f72c
DyAdmin <DyAdmin@w1k3z.onmicrosoft.com>
2024-06-30 16:58:34 +0800

    not thing

---
+++ Assets/Scripts/AllTheTest/TestRoslynAnalyzer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRoslynAnalyzer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 0;
+        if (a == 0)
+            a = 1;
+        else
+            a = 2;
+    }
+}
