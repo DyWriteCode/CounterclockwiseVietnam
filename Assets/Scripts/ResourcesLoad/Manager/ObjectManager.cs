ee99d3f5a011bb05168e7d37edbd96d1bd1705c7
DyAdmin <DyAdmin@w1k3z.onmicrosoft.com>
2024-08-29 12:24:21 +0800

    no commit message

--- Assets/Scripts/ResourcesLoad/Manager/ObjectManager.cs
+++ Assets/Scripts/ResourcesLoad/Manager/ObjectManager.cs
@@ -196,4 +196,91 @@ public class ObjectManager
             }
         }
     }
+
+    // 加载对象
+    // 异步加载
+    public long InstantiateObjectAsync(string path, OnAsyncObjFinish dealFinish, LoadResPriority priority = LoadResPriority.RES_MIDDLE, bool setSceneObject = false, object param1 = null, object param2 = null, object param3 = null, object param4 = null, object param5 = null, bool bClear = true)
+    {
+        if (string.IsNullOrEmpty(path) == true)
+        {
+            return 0;
+        }
+        uint crc = _CRC32.GetCRC32(path);
+        ResourceObj resourceObj = GetObjectFromPool(crc);
+        if (resourceObj != null)
+        {
+            if (setSceneObject == true)
+            {
+                resourceObj.m_cloneObj.transform.SetParent(SceneTrs, false);
+            }
+            if (dealFinish != null)
+            {
+                dealFinish.Invoke(path, resourceObj.m_cloneObj, param1, param2, param3, param4, param5);
+            }
+            return 0;
+        }
+        long guid = GameApp.ResourceManager.CreateGuid();
+        resourceObj = m_ResourceObjClassPool.Spawn(true);
+        resourceObj.m_crc = crc;
+        resourceObj.m_SetSceneParent = setSceneObject;
+        resourceObj.m_bClear = bClear;
+        resourceObj.m_DealFinish = dealFinish;
+        resourceObj.m_param1 = param1;
+        resourceObj.m_param2 = param2;
+        resourceObj.m_param3 = param3;
+        resourceObj.m_param4 = param4;
+        resourceObj.m_param5 = param5;
+        GameApp.ResourceManager.AsyncLoadResource(path, resourceObj, OnLoadResourceObjectFinish, priority);
+        return guid;
+    }
+
+    private void OnLoadResourceObjectFinish(string path, ResourceObj resourceObj, object param1 = null, object param2 = null, object param3 = null, object param4 = null, object param5 = null)
+    {
+        if (resourceObj == null)
+        {
+            return;
+        }
+        if (resourceObj.m_resItem.m_Object == null)
+        {
+#if UNITY_EDITOR
+            Debug.LogError("Load failed");
+#endif
+            return;
+        }
+        else
+        {
+            resourceObj.m_cloneObj = GameObject.Instantiate(resourceObj.m_resItem.m_Object) as GameObject;
+        }
+        if (resourceObj.m_cloneObj != null && resourceObj.m_SetSceneParent == true)
+        {
+            resourceObj.m_cloneObj.transform.SetParent(SceneTrs, false);
+        }
+        if (resourceObj.m_DealFinish != null)
+        {
+            int tempID = resourceObj.m_cloneObj.GetInstanceID();
+            if (m_ResourceObjDic.ContainsKey(tempID) == false)
+            {
+                m_ResourceObjDic.Add(tempID, resourceObj);
+            }
+            resourceObj.m_DealFinish.Invoke(path, resourceObj.m_cloneObj, resourceObj.m_param1, resourceObj.m_param2, resourceObj.m_param3, resourceObj.m_param4, resourceObj.m_param5);
+        }
+    }
+
+    // 预加载GameObject
+    public void PreLoadGameObject(string path, int count = 1, bool clear = false)
+    {
+        List<GameObject> tempGameObject = new List<GameObject>();
+        for (int i = 0; i < count; i++)
+        {
+            GameObject gameObject = InstantiateObject(path, false, clear);
+            tempGameObject.Add(gameObject);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            GameObject gameObject = tempGameObject[i];
+            ReleaseObject(gameObject);
+            gameObject = null;
+        }
+        tempGameObject.Clear();
+    }
 }
